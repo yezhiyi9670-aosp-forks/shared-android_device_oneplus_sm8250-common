98bdbb6110ed5ac8075042f441a90b5ca02badf1
Neokoni <dulin081682@126.com>
2026-04-16 06:30:23 +0000

    sm8250-common: add sepolicy for modified vibrator hal

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,8 @@ VENDOR_SECURITY_PATCH := 2024-10-05
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(COMMON_PATH)/sepolicy
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
